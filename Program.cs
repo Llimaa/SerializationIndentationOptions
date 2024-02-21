0f37af75af548604825e0defb63712dd6da0f199
Marcos Limal <marcoslimadefatima@gmail.com>
2024-02-21 00:11:33 -0300

    fix: fixed some errors

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
 ﻿using System.Text.Json;
 using SerializationIndentationOptions;
 
-Console.WriteLine("Agora, no .NET é possível formatar como desejamos serializar o json");
-Console.WriteLine("Digite 1 para identar seu código em Tabs");
-Console.WriteLine("Digite 2 para identar seu código em espaços");
-Console.WriteLine("Digite 3 para identar seu código com o Default web options");
+Console.WriteLine("Agora no .NET é possível formatar como desejamos serializar o json.");
+Console.WriteLine("Digite 1 para aplicar o recuo em Tabs.");
+Console.WriteLine("Digite 2 para aplicar o recuo em espaços.");
+Console.WriteLine("Digite 3 para aplicar o recuo com o Default web options.");
 
 var key = Console.ReadLine();
 var type = key == "1" ? "TABs": "Spaces";
 var indentSize = 0;
 
 if(key != "3") {
-    Console.WriteLine($"Agora informe a quantidade de espaçamento que você deseja { type }");
+    Console.WriteLine($"Agora informe a quantidade de recuo que você deseja { type }.");
     indentSize = int.Parse(Console.ReadLine() ?? "0");
 }
 
